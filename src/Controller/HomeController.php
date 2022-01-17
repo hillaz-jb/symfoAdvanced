99cc9d4692bf725112417789f0fa1c14fc721b24
Jérémy <jbarland@stagiaire-humanbooster.com>
2022-01-17 10:21:37 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        dump($user);
+        /*if ($user === null){
+            return $this->redirectToRoute('app_login');
+        }*/
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
